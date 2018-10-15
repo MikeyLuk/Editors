d170c20acce494adf5cf6d6a67c15c76c6667ea2
Andy <andymitchell1@live.co.uk>
2018-10-15 12:28:11 -0500

    First commit

---
+++ src/main/java/com/andymitchell/divedb/DiveDbApplication.java
@@ -0,0 +1,13 @@
+package com.andymitchell.divedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveDbApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DiveDbApplication.class, args);
+    }
+}
